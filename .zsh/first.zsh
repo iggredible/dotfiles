5457d5cd87708b4f2f250a2ed2f0f8834ecf7cd8
Igor Irianto <igor.irianto@gmail.com>
2025-12-30 16:09:30 -0600

    Consolidate rbenv codes into rbenv.zsh

--- .zsh/first.zsh
+++ .zsh/first.zsh
@@ -5,8 +5,3 @@ source $HOMEBREW_PREFIX/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 autoload -U promptinit; promptinit
 prompt pure # pure
 fpath+=("$(brew --prefix)/share/zsh/site-functions")
-
-
-# Rbenv
-FPATH=~/.rbenv/completions:"$FPATH"
-autoload -U compinit; compinit
--- .zsh/rbenv.zsh
+++ .zsh/rbenv.zsh
@@ -1,2 +1,4 @@
+FPATH=~/.rbenv/completions:"$FPATH"
+autoload -U compinit; compinit
 eval "$(rbenv init - zsh)"
 export PATH="$HOME/.rbenv/bin:$PATH"
